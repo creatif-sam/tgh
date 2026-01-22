4a69649d32df3a49c0082de8897d45cfee4a90a8
creatif-sam <samuel.creatiftech@gmail.com>
2026-01-22 17:39:16 +0100

    posts header correct

--- components/posts/MeditationButton.tsx
+++ components/posts/MeditationButton.tsx
@@ -1,17 +1,27 @@
 'use client'
 
 import { Button } from '@/components/ui/button'
+import { BookOpen } from 'lucide-react'
+import clsx from 'clsx'
 
-interface MeditationButtonProps {
+export interface MeditationButtonProps {
   onOpen: () => void
+  className?: string
 }
 
 export default function MeditationButton({
   onOpen,
+  className,
 }: MeditationButtonProps) {
   return (
-    <Button size="sm" variant="outline" onClick={onOpen}>
-      New Meditation
+    <Button
+      size="sm"
+      variant="outline"
+      onClick={onOpen}
+      className={clsx(className)}
+    >
+      <BookOpen className="h-4 w-4 mr-2" />
+      <span className="hidden sm:inline">Meditation</span>
     </Button>
   )
 }
