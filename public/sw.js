bd152d7a84ab1e75ef5178e0d69a5884e54ad8c0
creatif-sam <samuel.creatiftech@gmail.com>
2026-02-02 18:19:37 +0100

    notifications 6

--- public/sw.js
+++ public/sw.js
@@ -1,17 +1,22 @@
 // public/sw.js
 self.addEventListener('push', function (event) {
-  const data = event.data.json();
-  const options = {
-    body: data.body,
-    icon: '/icon-192x192.png',
-    badge: '/badge.png', // Small icon for mobile status bar
-    vibrate: [100, 50, 100],
-    data: { url: data.url }
-  };
+  if (event.data) {
+    const data = event.data.json(); // Assumes your server sends JSON
+    
+    const options = {
+      body: data.body,
+      icon: '/icons/icon-192x192.png', // Path to your app icon
+      badge: '/icons/badge-96x96.png', // Small white icon for status bar
+      vibrate: [100, 50, 100],
+      data: {
+        url: data.url || '/protected/posts', // Where to go when clicked
+      },
+    };
 
-  event.waitUntil(
-    self.registration.showNotification(data.title, options)
-  );
+    event.waitUntil(
+      self.registration.showNotification(data.title, options)
+    );
+  }
 });
 
 self.addEventListener('notificationclick', function (event) {
